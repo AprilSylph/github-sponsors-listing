759f5bce2f038fc6985a93217f4c0be158091c2e
April Sylph <28949509+AprilSylph@users.noreply.github.com>
2026-03-16 11:09:30 +0000

    JavaScript → TypeScript

---
+++ main.ts
@@ -0,0 +1,73 @@
+#!/usr/bin/env deno
+
+import { Octokit } from 'octokit';
+import { validate, type User } from '@octokit/graphql-schema';
+
+const query = `{
+  viewer {
+    sponsorsListing {
+      activeGoal {
+        description
+        kind
+        percentComplete
+        targetValue
+        title
+      }
+      featuredItems (featureableTypes: [REPOSITORY]) {
+        featureable {
+          ... on Repository {
+            description
+            languages (first: 1) {
+              nodes {
+                color
+                name
+              }
+            }
+            nameWithOwner
+            stargazerCount
+            url
+          }
+        }
+      }
+      shortDescription
+      sponsorable {
+        ... on User {
+          avatarUrl
+          location
+          login
+          name
+          url
+        }
+      }
+      url
+    }
+  }
+}`;
+
+const errors = validate(query);
+if (errors.length) {
+  console.error('❌ GraphQL query validation failed!');
+  errors.forEach(error => console.error(error.toString()));
+  Deno.exit(1);
+}
+
+const PERSONAL_ACCESS_TOKEN = Deno.env.get('PERSONAL_ACCESS_TOKEN');
+if (!PERSONAL_ACCESS_TOKEN) {
+  console.error('❌ PERSONAL_ACCESS_TOKEN environment variable is not set!');
+  Deno.exit(1);
+}
+
+try {
+  const octokit = new Octokit({ auth: PERSONAL_ACCESS_TOKEN });
+  const { viewer } = await octokit.graphql<{ viewer: User }>(query);
+
+  await Deno.mkdir('./out', { recursive: true });
+  await Deno.writeTextFile('./out/index.json', JSON.stringify(viewer, null, 2));
+
+  console.log('✅ Sponsors listing data saved to ./out/index.json');
+  Deno.exit(0);
+} catch (error) {
+  console.error('❌ Something went wrong:');
+  console.error(error);
+  Deno.exit(1);
+}
